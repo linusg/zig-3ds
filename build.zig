7f0932f939fb5ecff45bd5737dbce570e0e5915c
Linus Groh <mail@linusgroh.de>
2024-01-02 22:55:35 +0100

    Un-hardcode devkitpro path

--- build.zig
+++ build.zig
@@ -3,9 +3,14 @@ const builtin = @import("builtin");
 
 const emulator = "citra";
 const flags = .{"-lctru"};
-const devkitpro = "/opt/devkitpro";
 
 pub fn build(b: *std.build.Builder) void {
+    const devkitpro = std.process.getEnvVarOwned(b.allocator, "DEVKITPRO") catch |err| switch (err) {
+        error.EnvironmentVariableNotFound => @panic("Please set $DEVKITPRO"),
+        error.InvalidUtf8 => @panic("Invalid UTF-8"),
+        error.OutOfMemory => @panic("OOM"),
+    };
+
     const target: std.zig.CrossTarget = .{
         .cpu_arch = .arm,
         .os_tag = .freestanding,
@@ -24,32 +29,32 @@ pub fn build(b: *std.build.Builder) void {
     });
     obj.linkLibC();
     obj.setLibCFile(.{ .path = "libc.txt" });
-    obj.addIncludePath(.{ .path = devkitpro ++ "/libctru/include" });
-    obj.addIncludePath(.{ .path = devkitpro ++ "/portlibs/3ds/include" });
+    obj.addIncludePath(.{ .path = b.pathJoin(&.{ devkitpro, "libctru/include" }) });
+    obj.addIncludePath(.{ .path = b.pathJoin(&.{ devkitpro, "portlibs/3ds/include" }) });
 
     const obj_install = b.addInstallArtifact(obj, .{
         .dest_dir = .{ .override = .prefix },
     });
 
     const elf = b.addSystemCommand(&(.{
-        devkitpro ++ "/devkitARM/bin/arm-none-eabi-gcc" ++ extension,
+        b.pathJoin(&.{ devkitpro, "devkitARM/bin/arm-none-eabi-gcc" ++ extension }),
         "-g",
         "-march=armv6k",
         "-mtune=mpcore",
         "-mfloat-abi=hard",
         "-mtp=soft",
         "-Wl,-Map,zig-out/zig-3ds.map",
-        "-specs=" ++ devkitpro ++ "/devkitARM/arm-none-eabi/lib/3dsx.specs",
+        b.fmt("-specs={s}", .{b.pathJoin(&.{ devkitpro, "devkitARM/arm-none-eabi/lib/3dsx.specs" })}),
         "zig-out/zig-3ds.o",
-        "-L" ++ devkitpro ++ "/libctru/lib",
-        "-L" ++ devkitpro ++ "/portlibs/3ds/lib",
+        b.fmt("-L{s}", .{b.pathJoin(&.{ devkitpro, "libctru/lib" })}),
+        b.fmt("-L{s}", .{b.pathJoin(&.{ devkitpro, "portlibs/3ds/lib" })}),
     } ++ flags ++ .{
         "-o",
         "zig-out/zig-3ds.elf",
     }));
 
     const dsx = b.addSystemCommand(&.{
-        devkitpro ++ "/tools/bin/3dsxtool" ++ extension,
+        b.pathJoin(&.{ devkitpro, "tools/bin/3dsxtool" ++ extension }),
         "zig-out/zig-3ds.elf",
         "zig-out/zig-3ds.3dsx",
     });
