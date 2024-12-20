3f104d0e4ced54a97ddef03c9f9ec58a0413270e
Linus Groh <mail@linusgroh.de>
2024-12-20 00:43:51 +0000

    Use new std.Build API

--- build.zig
+++ build.zig
@@ -20,9 +20,11 @@ pub fn build(b: *std.Build) void {
 
     const obj = b.addObject(.{
         .name = "zig-3ds",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
     obj.linkLibC();
     obj.setLibCFile(b.path("libc.txt"));
